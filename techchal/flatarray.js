0fc524282e2743405c9cfdedca0e190130eb928a
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-01-25 16:53:58 -0500

    Refactor flatten to use spread

--- techchal/flatarray.js
+++ techchal/flatarray.js
@@ -1,19 +1,15 @@
 let input = [1, 2, 3, [[4], 5], [[[6]]]]
 
-let recursiveFunct = (input ,  index = 0, output = []) =>{
-  if (index === 5) return
-  if (output.lenth === input.length){
-    return output
-  }
-  if (typeof(input[index]) === "object"){
-    input[index].forEach(x => typeof(x) === "object" 
-      ? output.push(x) 
-      : recursiveFunct(x, 0, output))
-  }
-  else {
-    output.push(input[index])
-  }
-  index+=1
-  recursiveFunct(input, index, output)
+function flatten(items) {
+  const flat = [];
+
+  items.forEach(item => {
+    if (Array.isArray(item)) {
+      flat.push(...flatten(item));
+    } else {
+      flat.push(item);
+    }
+  });
+  return flat;
 }
-recursiveFunct(input)
+flatten(input)
