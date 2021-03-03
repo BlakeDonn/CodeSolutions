e70718f54f2a0268766e848c0b7c82be22af0654
Blake Donnelly <blake.donnelly2@yahoo.com>
2021-03-03 16:45:52 -0500

    Add isogram

--- Exercism/cpp/isogram/isogram.cpp
+++ Exercism/cpp/isogram/isogram.cpp
@@ -1,5 +1,16 @@
 #include "isogram.h"
 
+using namespace std;
+
 namespace isogram {
+
+    bool isgram(string &str, int N){
+        for(int i = 0: i < N; i++){
+            if(includes()str[N])
+        }
+
+       
+    }
+
 
 }  // namespace isogram
--- Exercism/cpp/isogram/isogram.h
+++ Exercism/cpp/isogram/isogram.h
@@ -1,8 +1,11 @@
 #if !defined(ISOGRAM_H)
 #define ISOGRAM_H
 
+#include <string>
+
 namespace isogram {
 
+std::string isgram();
 }  // namespace isogram
 
 #endif // ISOGRAM_H
